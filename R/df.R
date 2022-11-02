b32d86ca9c45b58fe3ef6f8cd9a952e23652ad55
Kirill Müller <krlmlr+r@mailbox.org>
2022-11-02 15:23:46 +0100

    Pass R CMD check

--- R/df.R
+++ R/df.R
@@ -19,61 +19,49 @@ rel_to_df.relational_df <- function(rel, ...) {
 
 #' @export
 rel_filter.relational_df <- function(rel, exprs, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_project.relational_df <- function(rel, exprs, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_aggregate.relational_df <- function(rel, groups, aggregates, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_order.relational_df <- function(rel, orders, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_inner_join.relational_df <- function(left, right, conds, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_limit.relational_df <- function(rel, n, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_distinct.relational_df <- function(rel, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_tostring.relational_df <- function(rel, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_explain.relational_df <- function(rel, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_alias.relational_df <- function(rel, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_set_alias.relational_df <- function(rel, alias, ...) {
-  stop("NYI")
 }
 
 #' @export
 rel_names.relational_df <- function(rel, ...) {
-  stop("NYI")
 }
 
