27709a33d22e544043574688141da05f74e6ec8b
Kirill Müller <krlmlr+r@mailbox.org>
2022-11-22 14:23:25 +0100

    Avoid message

--- R/altrep-wrap.R
+++ R/altrep-wrap.R
@@ -8,8 +8,6 @@ wrap_df <- function(df) {
   stopifnot(is.data.frame(df))
   out <- lapply(df, wrap)
   class(out) <- "data.frame"
-  message(1)
   .Call(set_row_names, out, wrap(.set_row_names(nrow(df))))
-  message(2)
   out
 }
